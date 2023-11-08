16f8bcd6107e5f7b66979229f83722bda5b20364
zk <longbowk@yeah.net>
2023-11-08 19:15:25 +0800

    [feature] add utils.FindAllPrefixMatch

--- utils/string.go
+++ utils/string.go
@@ -1,5 +1,10 @@
 package utils
 
+import (
+	"sort"
+	"strings"
+)
+
 // Much efficient than regexp, about 20x
 
 // IsAllNumbers return true if input contains only numbers.
@@ -30,4 +35,29 @@ func IsAllNumberLetters(input string) bool {
 		}
 	}
 	return true
+}
+
+// FindAllPrefixMatch return all item in sorted string slice has the specified prefix.
+func FindAllPrefixMatch(ss []string, prefix string) []string {
+	n := len(ss)
+	prefixLen := len(prefix)
+	offset, found := sort.Find(n, func(i int) int {
+		if len(ss[i]) >= prefixLen {
+			return strings.Compare(prefix, ss[i][:prefixLen])
+		} else {
+			return 1
+		}
+	})
+	if !found {
+		return nil
+	}
+
+	var result []string
+	for i := offset; i < n; i++ {
+		if !strings.HasPrefix(ss[i], prefix) {
+			break
+		}
+		result = append(result, ss[i])
+	}
+	return result
 }
