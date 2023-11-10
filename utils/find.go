6ea201736641bcb2308cacd4dac4af6b02140d99
zk <longbowk@yeah.net>
2023-11-10 22:13:37 +0800

    [feature] utils: add FindFirst() and FindLast()

--- utils/find.go
+++ utils/find.go
@@ -3,11 +3,13 @@ package utils
 import "sort"
 
 // FindFirst return the first index that pred(i) is true.
+// If not found, return n.
 func FindFirst(n int, pred func(i int) bool) int {
 	return sort.Search(n, pred)
 }
 
-// FindLast return the last index that pred(i) is true
+// FindLast return the last index that pred(i) is true.
+// If not found, return -1.
 func FindLast(n int, pred func(i int) bool) int {
 	reverseOffset := sort.Search(n, func(i int) bool {
 		j := n - i - 1
