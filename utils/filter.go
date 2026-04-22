15420f241842d81c74c54f60e2299bbdcf870068
zhangkai <zhangkai@weixinkd.com>
2026-04-22 14:17:01 +0800

    add FilterSlice

--- utils/filter.go
+++ utils/filter.go
@@ -6,3 +6,16 @@ func Filter(params map[string]any, filters []string) {
 		delete(params, f)
 	}
 }
+
+// FilterSlice remove specified element
+func FilterSlice[S ~[]E, E comparable](slice S, value E) S {
+	result := make(S, 0, len(slice))
+
+	for _, item := range slice {
+		if item != value {
+			result = append(result, item)
+		}
+	}
+
+	return result
+}
